19b60c50e391c86ebb89cc4cb7278613376198d4
Orlando Andrade <orlando.andrade1995@gmail.com>
2018-12-16 16:32:11 -0300

    escritura

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,5 +1,8 @@
 import com.google.gson.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Map;
 
 public class Paciente {
@@ -58,44 +61,17 @@ public class Paciente {
         return "Ok";
     }
 
-    public void requerimiento_externo(int llave, String accion, String cargo){
-        String[] partes = accion.split(" ");
-        if (partes[0].equals("recetar") && cargo.equals("doctor")){
-            JsonObject paciente = this.datos_paciente(llave);
-            JsonPrimitive medicamento = new JsonPrimitive(partes[1]);
-            ((JsonObject) paciente.getAsJsonArray("medicamentos").get(0)).getAsJsonArray("recetados").add(medicamento);
-
+    public void escribir_pacientes(String path){
+        try {
+            Writer file = new FileWriter(path);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(this.datos_pacientes);
+            file.write(json);
+            file.close();
+            System.out.println("Actualizacion correcta");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        else if (partes[0].equals("pedir") && cargo.equals("doctor")){
-            JsonObject paciente = this.datos_paciente(llave);
-            JsonPrimitive examen = new JsonPrimitive(partes[1]);
-            ((JsonObject) paciente.getAsJsonArray("examenes").get(1)).getAsJsonArray("no realizados").add(examen);
-        }
-
-        else if (partes[0].equals("realizar") && (cargo.equals("doctor") || cargo.equals("paramedico"))){
-            JsonObject paciente = this.datos_paciente(llave);
-            JsonPrimitive examen = new JsonPrimitive(partes[1]);
-            ((JsonObject) paciente.getAsJsonArray("examenes").get(1)).getAsJsonArray("realizados").add(examen);
-        }
-
-        else if (partes[0].equals("colocar") && (cargo.equals("doctor") || cargo.equals("enfermero"))){
-            JsonObject paciente = this.datos_paciente(llave);
-            JsonPrimitive procedimiento = new JsonPrimitive(partes[1]);
-            ((JsonObject) paciente.getAsJsonArray("tratamientos/procedimientos").get(1)).getAsJsonArray("completados").add(procedimiento);
-        }
-
-        else if (partes[0].equals("asignar") && cargo.equals("doctor")){
-            JsonObject paciente = this.datos_paciente(llave);
-            JsonPrimitive procedimiento = new JsonPrimitive(partes[1]);
-            ((JsonObject) paciente.getAsJsonArray("tratamientos/procedimientos").get(1)).getAsJsonArray("asignados").add(procedimiento);
-        }
-
-        else if (partes[0].equals("suministrar") && (cargo.equals("doctor") || cargo.equals("enfermero"))){
-            JsonObject paciente = this.datos_paciente(llave);
-            JsonPrimitive medicamento = new JsonPrimitive(partes[1]);
-            ((JsonObject) paciente.getAsJsonArray("medicamentos").get(1)).getAsJsonArray("suministrados").add(medicamento);
-        }
-
     }
 
 }
